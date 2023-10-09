88b52da5cb907ca619e09d9cb9097c8fdf80d40f
qzh <2806894439@qq.com>
2023-10-09 22:25:01 +0800

    WorkQueue任务模型

--- mq-demo/consumer/src/main/java/cn/itcast/mq/listener/SpringRabbitListener.java
+++ mq-demo/consumer/src/main/java/cn/itcast/mq/listener/SpringRabbitListener.java
@@ -3,10 +3,24 @@ package cn.itcast.mq.listener;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalTime;
+
 @Component
 public class SpringRabbitListener {
-    @RabbitListener(queues = "simple.queue")
+    /*@RabbitListener(queues = "simple.queue")
     public void listenSimpleQueue(String msg) {
         System.out.println("消费者接收到simple.queue的消息：【" + msg + "】");
+    }*/
+
+    @RabbitListener(queues = "simple.queue")
+    public void listenWorkQueue1(String msg) throws InterruptedException {
+        System.out.println("消费者 1 接收到消息：【" + msg + "】" + LocalTime.now());
+        Thread.sleep(20);
+    }
+
+    @RabbitListener(queues = "simple.queue")
+    public void listenWorkQueue2(String msg) throws InterruptedException {
+        System.err.println("消费者 2 接收到消息：【" + msg + "】" + LocalTime.now());
+        Thread.sleep(200);
     }
 }
