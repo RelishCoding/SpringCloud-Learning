80650d145c881ce3d0e0020b63e957c1fa46f1e4
qzh <2806894439@qq.com>
2023-12-29 22:55:11 +0800

    hotel-demo初始化工程

---
+++ hotel-demo/src/test/java/cn/itcast/hotel/HotelDemoApplicationTests.java
@@ -0,0 +1,11 @@
+package cn.itcast.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HotelDemoApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
